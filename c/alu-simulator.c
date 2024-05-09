938c6159f27fb16d4878cb58841726778a4a4c11
nerogdev <nerog.dev@gmail.com>
2024-05-09 20:57:05 +0200

    feat: printing as binary format. Exponent and mantissa. Besides, adds some constants.

--- c/alu-simulator.c
+++ c/alu-simulator.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+const int MANTISSA_BITS = 23;
+const int EXPONENT_BITS = 8;
+
 typedef union {
     float f;
     struct {
@@ -24,7 +27,7 @@ Float_t parseFloat(int num) {
     }
 
     fnum.parts.exponent = mostSignificantBit + 127; // bias to represent -126 to 127 in binary mode.
-    fnum.parts.mantissa = (num << (23 - mostSignificantBit)) & 0x7FFFFF; // shift to the left due to the most significant bit and apply a mask to ensure that we keep the less significant 23 bits 
+    fnum.parts.mantissa = (num << (MANTISSA_BITS - mostSignificantBit)) & 0x7FFFFF; // shift to the left due to the most significant bit and apply a mask to ensure that we keep the less significant 23 bits 
     return fnum;
 }
 
@@ -37,13 +40,23 @@ void printFloat(Float_t *fnum) {
     printf("{\n");
     printf("  \"number\": %f,\n", fnum->f);
     printf("  \"sign\": %d,\n", fnum->parts.sign);
-    printf("  \"exponent\": %d,\n", fnum->parts.exponent);
-    printf("  \"mantissa\": %d\n", fnum->parts.mantissa);
-    printf("}\n");
+    
+    printf("  \"exponent\": ");
+    for (int i = EXPONENT_BITS - 1; i >= 0; i--)
+    {
+        printf("%d", fnum->parts.exponent >> i & 1);
+    }
+    
+    printf(",\n  \"mantissa\": ");
+    for (int i = MANTISSA_BITS - 1; i >= 0; i--)
+    {
+        printf("%d", fnum->parts.mantissa >> i & 1);
+    }
+    printf("\n}\n");
 }
 
 int main(int argc, char *argv[]) {
-    Float_t fnum = parseFloat(60);
+    Float_t fnum = parseFloat(-5);
     printFloat(&fnum);
 
     fnum = parseFloat(10);
